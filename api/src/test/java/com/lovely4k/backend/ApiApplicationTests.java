f9a72ffcdae30faf5ffa9200e0978109c9c7dd82
BeomSeogKim <kbs4520@daum.net>
2023-10-18 16:43:19 +0900

    chore : 초기설정

---
+++ api/src/test/java/com/lovely4k/backend/ApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lovely4k.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
