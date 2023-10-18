c10e59face7bc3d5a6293bf575ea468a50be0e76
joyfulviper <kimsk3113@naver.com>
2023-10-18 23:22:25 +0900

    test : 11

--- api/src/test/java/com/lovely4k/backend/Sonar.java
+++ api/src/test/java/com/lovely4k/backend/Sonar.java
@@ -1,6 +1,7 @@
 package com.lovely4k.backend;
 
 import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
 class Sonar {
@@ -10,6 +11,7 @@ class Sonar {
         Assertions.assertThat("a").isEqualTo("a");
     }
 
+    @Disabled
     @Test
     void test2() {
         Assertions.assertThat("a").isEqualTo("b");
